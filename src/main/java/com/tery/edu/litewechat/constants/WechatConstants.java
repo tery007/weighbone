a2e458b383d37fe0de7df1ce7a6c2f6aea641637
wanglei <wanglei1@meihaofenqi.com>
2018-08-29 22:03:58 +0800

    ..

--- src/main/java/com/tery/edu/litewechat/constants/WechatConstants.java
+++ src/main/java/com/tery/edu/litewechat/constants/WechatConstants.java
@@ -7,8 +7,8 @@ public class WechatConstants {
 
     public static final String ACCESS_TOKEN_URL = "https://com.github.sd4324530.fastweixin.api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET";
 
-    public static final String APP_ID = "wxa09bebf121d2508a";
-    public static final String SECRET = "34d58de5a4f94bda8ae66c0203225601";
+    public static final String APP_ID = "wx104955e13af82c54";
+    public static final String SECRET = "16ed45e3832cd9056c270be3974a7739";
     public static final String TOKEN  = "impxjgj34age";
 
 }
