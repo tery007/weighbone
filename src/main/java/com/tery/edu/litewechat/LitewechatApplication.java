65c560388b6e677dfde5b31ad5bae70e597bb909
wanglei <wanglei1@meihaofenqi.com>
2019-03-08 16:30:45 +0800

    ..

--- src/main/java/com/tery/edu/litewechat/LitewechatApplication.java
+++ src/main/java/com/tery/edu/litewechat/LitewechatApplication.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.client.RestTemplate;
@@ -21,6 +22,7 @@ public class LitewechatApplication {
 
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
